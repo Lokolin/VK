35528adc56dd2c84e6577d0175e1da5207222c82
Dmitry <game_guru@mail.ru>
2018-06-28 00:20:28 +0300

    Создал GroupService  добавил в него метод получения всех пользователей

--- VK Analysis Console/GroupService.cs
+++ VK Analysis Console/GroupService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using VkNet;
 using VkNet.Enums.Filters;
+using VkNet.Model;
 using VkNet.Model.RequestParams;
 
 namespace VK_Analysis_ConsoleAnalysisAnalysis
@@ -17,18 +18,30 @@ namespace VK_Analysis_ConsoleAnalysisAnalysis
 
         public void GetGroupMembersInfo(string groupId)
         {
-            var members = vkApi.Groups.GetMembers(new GroupsGetMembersParams
+            List<User> allUsers = new List<User>();
+            int i = 0;
+            var capacity = vkApi.Groups.GetMembers(new GroupsGetMembersParams
             {
                 GroupId = groupId,
-                Fields = UsersFields.City
             });
-            foreach (var member in members)
+            int count = Convert.ToInt32(capacity.TotalCount) ;
+            while (i<count)
             {
-                Console.WriteLine(member.FirstName + " " + member.LastName + " " + member.City?.Title);
+                var members = vkApi.Groups.GetMembers(new GroupsGetMembersParams
+                {
+                    GroupId = groupId,
+                    Offset = i,
+                    Fields = UsersFields.City 
+                });
+                foreach (var member in members)
+                {
+                    Console.WriteLine(member.FirstName + " " + member.LastName + " " + member.City?.Title);
+                    allUsers.Add(member);
+                }
+                i += 1000;
             }
 
-            Console.WriteLine("Число подписчкиов в группе" + members.Count);
-
+            Console.WriteLine("Число подписчиков в группе" + allUsers.Count);
         }
     }
 }
